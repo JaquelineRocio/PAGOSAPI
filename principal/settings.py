03ce0e437c789ec8c175a586aaf8279ef6a55fa6
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-12-20 22:54:04 -0500

    refactorizando y agregando los modelos services y expired payments

--- principal/settings.py
+++ principal/settings.py
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'users',
     'pagos',
-    'versionedPagos'
+    'versionedPagos',
+    'expired_payments',
+    'services'
+    
 ]
 
 MIDDLEWARE = [
