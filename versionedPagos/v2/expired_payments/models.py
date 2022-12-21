e2d3da6f955268a549d65c93ac62e7c23c782812
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-12-20 21:53:58 -0500

    Creando las apps services, expired_payments, payment_user y refactorizando el orden de los archivos

---
+++ versionedPagos/v2/expired_payments/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from pagos.models import Pago
+
+class ExpiredPayment(models.Model):
+   
+    pago = models.ForeignKey(Pago, on_delete=models.CASCADE)
+    penalty_free_amount = models.FloatField(default=0.0)
+
+    def __str__(self):
+       
+        return self.pago.fecha_pago
