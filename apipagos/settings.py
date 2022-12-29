851c4440605d2d4fd1d07e8ee53e378f629a8e51
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-12-29 00:40:32 -0500

    all host allowed

--- apipagos/settings.py
+++ apipagos/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o8aw75jq^i3i0&pon@!&msbdsz8b!=zd=7=4uz-1-yv16@61bm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
