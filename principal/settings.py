7e8dde3a12bf0309170a78d6e3659139cba5aa99
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-12-28 21:17:10 -0500

    configurando para el deploy v1

--- principal/settings.py
+++ principal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wtqmd8ifb%7ksovbu4ubigf=zdooq+c+y6sc$af8^ikhe)3_@k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
